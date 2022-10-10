8027ee1913315bd976c8d51b110de6d4f800cb2b
sergey klykov <sergei_klykov@epam.com>
2022-10-10 17:15:30 +0200

    add temporary access control

--- src/lambdas/getProductById/index.js
+++ src/lambdas/getProductById/index.js
@@ -19,5 +19,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify(result),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
     };
 };
--- src/lambdas/getProductsList/index.js
+++ src/lambdas/getProductsList/index.js
@@ -9,5 +9,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify(result),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
     };
 };
